1af689c366c69f9735566f5fcfbaeb32e2ccb6e7
Priyanka <priyankaarikatla696@gmail.com>
2022-10-10 09:06:18 +0530

    update

--- myshop/index.js
+++ myshop/index.js
@@ -1,4 +1,4 @@
-console.log(screen.width)
+
 var myIndex = 0;
 carousel();
 
@@ -14,4 +14,20 @@ function carousel() {
   // console.log(x[myIndex - 1]);
   // console.log(myIndex)
   setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+var myIndex1 = 0;
+carousel1();
+
+function carousel1() {
+  var i;
+  var x1 = document.getElementsByClassName("mySlides1");
+  for (i = 0; i < x.length; i++) {
+    x1[i].style.display = "none";
+  }
+  myIndex1++;
+  if (myIndex1 > x1.length) { myIndex1 = 1 }
+  x1[myIndex1 - 1].style.display = "block";
+  // console.log(x[myIndex - 1]);
+  // console.log(myIndex)
+  setTimeout(carousel1, 2000); // Change image every 2 seconds
 }
